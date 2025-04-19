c408dc9fa55bf2f3500ba92c0dfa4521cadf44ba
BigBabyofTel <augustus.tb@outlook.com>
2025-04-19 19:14:10 -0400

    tailwind issue fixed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {
+      plugins: [
+        'tailwindcss',
+        'autoprefixer'
+      ]
+    }
   },
 };
 
